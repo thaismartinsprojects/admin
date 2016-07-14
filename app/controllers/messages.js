2de5da0dd4363c8e2236c90c91e5ff3234d8d3ee
Thais Martins <contato@thaismartins.co>
2016-07-13 22:12:15 -0300

    Removing modal messages

--- app/controllers/messages.js
+++ app/controllers/messages.js
@@ -3,6 +3,8 @@
 angular.module('adminThaisMartins')
 .controller('MessagesController', ['$scope', function ($scope) {
 
+    $scope.visibleSearch = false;
+
     $scope.openNewMessage = function() {
         angular.element('#messages').modal({backdrop: 'static', keyboard: false});
     };
@@ -13,5 +15,9 @@ angular.module('adminThaisMartins')
 
     $scope.sendMessage = function() {
         
+    };
+
+    $scope.showSearch = function() {
+        $scope.visibleSearch = !$scope.visibleSearch;
     };
 }]);
